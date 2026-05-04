952bdee955eb03f2d2bde857a73483ce9652a946
Guillaume Lambert <guil.lambert@protonmail.com>
2026-05-04 12:19:20 -0400

    chore: use max_tokens <64K

--- packages/validator/src/cli/autorun-generate.ts
+++ packages/validator/src/cli/autorun-generate.ts
@@ -106,7 +106,7 @@ async function main(): Promise<number> {
     try {
       const resp = await client.messages.create({
         model: args.model,
-        max_tokens: 65536,
+        max_tokens: 63999,
         system: [
           {
             type: "text",
